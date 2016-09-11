4656fdf483f6cfb10f38cb2b87d06d8429b2c557
syzer3@gmail.com <syzer3@gmail.com>
2016-09-11 11:14:32 +0200

    messages

--- app/messages/messages.service.js
+++ app/messages/messages.service.js
@@ -6,7 +6,18 @@ class MessageService {
 
   getMessages() {
     return $q.when([
-
+      {
+        img: 'https://pbs.twimg.com/media/BqyHXLkCQAEE2hf.jpg',
+        text: 'reader will be distracted by the readable content of a page when looking at  web page editors now use Lorem Ipsum as their default model text, and a search',
+        notify: 'me',
+        blocked: false,
+      },
+      {
+        img: 'https://pbs.twimg.com/media/BqyHXLkCQAEE2hf.jpg',
+        text: 'reader will be distracted by the readable content of a page when looking at  web page editors now use Lorem Ipsum as their default model text, and a search',
+        notify: 'allerts',
+        blocked: false,
+      }
     ]);
   }
 }
